c807a5c6d94a1f01d7d9c374b8621e7cc59b1bf4
Jisun696 <istiakkashem35@gmail.com>
2024-05-15 01:10:27 +0600

    integrated jwt

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -13,6 +15,23 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use(cookieParser());
+
+// verify jwt middleware
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+  if (!token) return res.status(401).send({ message: 'Not Authorized' });
+
+  jwt.verifyToken(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      console.log(err);
+      return res.status(401).send({ message: 'Not Authorized' });
+    }
+    console.log('values in the token: ', decoded);
+    req.decodedUser = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.chn7ebi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,6 +47,19 @@ async function run() {
     const blogsCollection = client.db('BitsreamDB').collection('blogs');
     const wishlistCollection = client.db('BitsreamDB').collection('wishlist');
     const commentCollection = client.db('BitsreamDB').collection('comment');
+
+    // generate jwt
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '365d' });
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        })
+        .send({ success: true });
+    });
 
     app.get('/blogs', async (req, res) => {
       const cursor = blogsCollection.find({});
